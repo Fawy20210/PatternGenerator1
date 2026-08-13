5ab867bc1353aa26458723652aa033c4d95806cb
Fabian <wyssfabian1@gmail.com>
2026-08-13 21:09:41 +0200

    added inputs and the ability to generate patterns using them

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/* const resxInput = Number(document.getElementById("resx").textContent);
+const resyInput = Number(document.getElementById("resy").textContent);
+const objectCount = Number(document.getElementById("objectCount").textContent);
+const iterations = Number(document.getElementById("download").textContent); */
+
+const checkbox = document.getElementById("mode");
+const startingPointsContainer  = document.getElementById("startingPoints-container");
+const generateButton = document.getElementById("generate");
 const download = document.getElementById("download");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -18,8 +26,7 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-const CanvasWidth = canvas.clientWidth;
-const CanvasHeight = canvas.clientHeight;
+
 
 
 let ObjectCount = 2;
@@ -32,32 +39,14 @@ let objects = [
     /* new Point(250,250,-1,0),
     new Point(250,250,1,0),
     new Point(250,250,0,-1),
-    new Point(250,250,0,1), */
+    new Point(250,250,0,1), *//* 
     new Point(CanvasWidth/2,CanvasHeight/2,0,-1),
     new Point(CanvasWidth/2,CanvasHeight/2,0,1),
     new Point(CanvasWidth/2,CanvasHeight/2/2,1,0),
-    new Point(CanvasWidth/2,CanvasHeight/2,-1,0),
+    new Point(CanvasWidth/2,CanvasHeight/2,-1,0), */
 
 ];
 
-
-if(randomStarts){
-    objects = [];
-    for(let i=0; i<ObjectCount; i++){
-        let x = getRandomArbitrary(0,CanvasWidth);
-        let y = getRandomArbitrary(0,CanvasHeight);
-
-        let d = getRandomIntInclusive(0,3);
-        let dx = 0;
-        let dy = 0;
-        if(d == 0) dx = 1;
-        if(d == 1) dx = -1;
-        if(d == 2) dy = 1;
-        if(d == 3) dy = -1;
-
-        objects.push(new Point(x,y,dx,dy));
-    }
-}
 
 
 
@@ -119,11 +108,58 @@ let inter = setInterval(()=>{
 
 }, 1000); */
 /* 
-ctx.scale(CanvasWidth/MyWidth,CanvasHeight/MyHeight); */
+ctx.scale(CanvasWidth/MyWidth,CanvasHeight/MyHeight);
+*/
 
-for(let i=0; i<iterations; i++){
+/* for(let i=0; i<iterations; i++){
     draw(i+1);
-} 
+}  */
+
+checkbox.addEventListener("click", () => {
+    if(checkbox.checked) startingPointsContainer.style.display="block";
+    else startingPointsContainer.style.display="none";
+});
+
+generateButton.addEventListener("click", () => {
+    canvas.width = document.getElementById("resx").value;
+    canvas.height = document.getElementById("resy").value;
+    objectCount = document.getElementById("objectCount").value;
+    iterations = document.getElementById("iterations").value;
+
+    let CanvasWidth = canvas.clientWidth;
+    let CanvasHeight = canvas.clientHeight;
+    ctx.clearRect(0,0,CanvasWidth,CanvasWidth);
+    if(checkbox.checked){
+        objects = [];
+        for(let i=0; i<ObjectCount; i++){
+            let x = getRandomArbitrary(0,CanvasWidth);
+            let y = getRandomArbitrary(0,CanvasHeight);
+
+            let d = getRandomIntInclusive(0,3);
+            let dx = 0;
+            let dy = 0;
+            if(d == 0) dx = 1;
+            if(d == 1) dx = -1;
+            if(d == 2) dy = 1;
+            if(d == 3) dy = -1;
+
+            objects.push(new Point(x,y,dx,dy));
+        }
+    } else{
+        objects = [
+            new Point(CanvasWidth/2,CanvasHeight/2,0,-1),
+            new Point(CanvasWidth/2,CanvasHeight/2,0,1),
+            new Point(CanvasWidth/2,CanvasHeight/2/2,1,0),
+            new Point(CanvasWidth/2,CanvasHeight/2,-1,0),
+        ]
+    }
+
+    //canvas.style.display = "block";
+    for(let i=0; i<iterations; i++){
+        draw(i+1);
+    }
+
+});
 
 download.addEventListener("click", () => {
     canvas.toBlob((blob) => {
